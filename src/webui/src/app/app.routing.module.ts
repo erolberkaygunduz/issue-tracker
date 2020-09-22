a8e1212e70563b0e012310da6ad10f5c0599eff6
Berkay Gündüz <l1321012003@stud.sdu.edu.tr>
2020-09-22 22:32:59 +0300

    V1.3

--- src/webui/src/app/app.routing.module.ts
+++ src/webui/src/app/app.routing.module.ts
@@ -1,21 +1,20 @@
-import {NgModule} from "@angular/core";
 import {RouterModule,Routes} from "@angular/router";
-import {AppComponent} from "./app.component";
+import {NgModule} from "@angular/core";
 
 const routes: Routes = [
   {
-    path:'',
-    children:[
-      {path: '',pathMatch:'full',redirectTo:'issue'},
-      {path: 'dashboard',loadChildren: './pages/,issue/dashboard.module#DashboardModule'},
-      {path: 'issue',loadChildren: './pages/,issue/issue.module#IssueModule'},
-      {path: 'project',loadChildren: './pages/,issue/project.module#ProjectModule'}
+    path: '',
+    children: [
+      {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
+      {path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardModule'},
+      {path: 'issue', loadChildren: './pages/issue/issue.module#IssueModule'},
+      {path: 'project', loadChildren: './pages/project/project.module#ProjectModule'}
     ]
-  }
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 
